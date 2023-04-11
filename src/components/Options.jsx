001b7924eeeeff3af9259e3e29576f97e7465ab1
Mica <micabeacabrera@gmail.com>
2023-04-11 01:41:53 -0300

    details

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -5,9 +5,9 @@ export const Options = ({handleFilters}) => {
  
   return (
     <Select onChange={handleFilters} size='md' width={200} bgColor="whiteAlpha.800" rounded='md'>
-        <option value="all">Todas</option>
-        <option value="complete">Completas</option>
-        <option value="incomplete">Incompletas</option>
+        <option value="all">All</option>
+        <option value="complete">Complete</option>
+        <option value="incomplete">Incomplete</option>
       </Select>
   );
 };
